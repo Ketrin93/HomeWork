9a3136a8f887d34cee9c74c6d4dd6b828dadabab
hello <lazareva_ekaterina_96_96@mail.ru>
2024-12-27 00:39:22 +0300

    сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,78 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {System.out.println("Задание 1");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        System.out.println("Задание 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper +4;
+        System.out.println(paper);
+        System.out.println("Задание 3");
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        System.out.println("Задание 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend );
+        System.out.println("Задание 5");
+        var frog = 3.5;
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        System.out.println("Задание 7");
+        var  firstBoxerWeight = 78.2;
+        var secondBoxerWeight =82.7;
+        System.out.println(secondBoxerWeight % firstBoxerWeight );
+        System.out.println("Задание 6");
+        System.out.println(secondBoxerWeight + firstBoxerWeight );
+        System.out.println(secondBoxerWeight - firstBoxerWeight );
+        System.out.println("Задание 8");
+        var totalWorkTime =640;
+        var workTimeOnePeople = 8;
+        System.out.println(totalWorkTime /workTimeOnePeople );
+        var workerCompany = 80;
+        workerCompany = workerCompany + 94;
+        var totalWorkHours = workerCompany * workTimeOnePeople ;
+        System.out.println(" Если в компании работает"+ workerCompany + "человек, то всего "+ totalWorkHours  + " часов работы может быть поделено между сотрудниками .");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
+
